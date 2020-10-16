1e2e2e69c156780af18e0c09cee721d4f92d191a
sudaratchoothong <62239375+sudaratchoothong@users.noreply.github.com>
2020-10-16 14:53:14 +0700

    commit

--- firebase.js
+++ firebase.js
@@ -5,9 +5,9 @@ var firebaseConfig = {
     projectId: "meowflix-9bd3d",
     storageBucket: "meowflix-9bd3d.appspot.com",
     messagingSenderId: "887706623073",
-    appId: "1:429650509455:web:adadc8b6215fe21011fafa",
-    measurementId: "G-HG7G7ZEFPC"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+    appId: "1:429650509455:web:adadc8b6215fe21011fafa", //ยังหาapp ในเบสไม่เจอ ที่เขียนไปเป็นของอใจารย์
+    measurementId: "G-HG7G7ZEFPC" //อันนี้ด้วย
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
